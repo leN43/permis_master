679c1e7091eb8eaab8f3f31e21d02ded7d1474f8
Niels Ayigbede <niels.ayigbede@gmail.com>
2023-10-14 14:21:48 +0200

    avant deploy

--- app/controllers/contact_form_controller.rb
+++ app/controllers/contact_form_controller.rb
@@ -1,13 +1,13 @@
 class ContactFormController < ApplicationController
   def create
-    @name = params[:contact_form][:name]
+    @name = params[:contact_form][:nom]
     @last_name = params[:contact_form][:last_name]
     @email = params[:contact_form][:email]
     @message = params[:contact_form][:message]
-    @subject = params[:contact_form][:subject]
+    @objet = params[:contact_form][:objet]
 
     # Perform any necessary actions with the form data
-    flash[:success] = "Your message has been sent successfully."
-    redirect_to :informations
+    flash[:success] = 'Message bien envoyé !'
+    redirect_to :contact
 end
 end
