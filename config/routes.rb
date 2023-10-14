ed8adb8e77a38f3541e3e7058ab1dfe9e8ee65af
Niels Ayigbede <niels.ayigbede@gmail.com>
2023-10-14 11:44:54 +0200

    page conact fin

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
    root "pages#home"
-   get 'contact' => 'pages#contact'
    get 'formations' => 'pages#formations'
-   get 'informations' => 'contact_form#new'
+   get 'contact' => 'contact_form#new'
    resources :contact_form, only: [:new, :create]
 end
