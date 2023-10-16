5b455928d9cb1b49e4d34506e693ce435c91d463
Niels Ayigbede <niels.ayigbede@gmail.com>
2023-10-17 01:14:48 +0200

    mobile toggler fix

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,2 +1,8 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
+const collapseBtn = document.getElementById("navbarCollapse")
+
+collapseBtn.addEventListener("click" ,() =>{
+
+  console.log('hello');
+});
